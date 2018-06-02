04da435db4f2f7496babb43993b62e225f267f6b
Natasha Ramburrun <r_natasha100@hotmail.com>
2018-06-02 19:09:37 +0100

    adding bulma to the layout ejs page

--- server.js
+++ server.js
@@ -6,13 +6,18 @@ const app = express();
 //
 // app.get('/', (req, res)=> res.send('<h1>Hello World!</h1>'));
 
-//handle a request
-app.get('/',(req, res) => res.render('home'));
 
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/views`);
 
 app.use(ejsLayouts);
+
+app.use(express.static(`${__dirname}/public`));
+
+//handle a request
+app.get('/',(req, res) => res.render('home', {
+  isHomepage: true
+}));
 
 //listen out of the incoming requests on PORT 4000
 app.listen(4000, ()=> console.log('Express is listening to port 4000'));
