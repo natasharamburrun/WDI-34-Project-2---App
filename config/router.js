9262bdc9c31bac16296bc175c839f9189b15f771
Natasha Ramburrun <r_natasha100@hotmail.com>
2018-06-08 22:08:05 +0100

    made changes to the hairdresser controls so they could only access this also added an about page

--- config/router.js
+++ config/router.js
@@ -7,6 +7,7 @@ const portfolios = require('../controllers/portfolios');
 const pictures = require('../controllers/pictures');
 
 router.get('/', (req, res) => res.render('home', { isHomepage: true }));
+router.get('/about', (req, res) => res.render('pages/about'));
 
 router.route('/login')
   .get(sessions.new)
