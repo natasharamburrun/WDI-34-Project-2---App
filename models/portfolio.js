b657abe0748b33b9696a03ebd1783e708689e348
Natasha Ramburrun <r_natasha100@hotmail.com>
2018-08-17 17:56:56 +0100

    Completed so bug fixes on the portfolio page

--- models/portfolio.js
+++ models/portfolio.js
@@ -5,15 +5,9 @@ const commentSchema = new mongoose.Schema({
   user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
 });
 
-// const pictureSchema = new mongoose.Schema({
-//   title: String,
-//   url: String,
-//   creator: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-//   comments: [commentSchema]
-// });
-
 const portfolioSchema = new mongoose.Schema({
   name: String,
+  occupation: String,
   salon: String,
   email: String,
   address: String,
@@ -22,6 +16,10 @@ const portfolioSchema = new mongoose.Schema({
   price: String,
   url: String,
   creator: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  location: {
+    lat: Number,
+    lng: Number
+  },
   // pictures: [pictureSchema]
   comments: [commentSchema]
 });
--- public/app.js
+++ public/app.js
@@ -1,20 +1,22 @@
+/* global google*/
+
 document.addEventListener('DOMContentLoaded', () => {
 
-  // Get all "navbar-burger" elements
+//***********************************************
+//Code to activate burger menu on mobile in BULMA
+// Add a click event on each burger menu
+//***********************************************
+
   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
-  // Check if there are any navbar burgers
   if ($navbarBurgers.length > 0) {
 
-    // Add a click event on each of them
     $navbarBurgers.forEach( el => {
       el.addEventListener('click', () => {
 
-        // Get the target from the "data-target" attribute
         const target = el.dataset.target;
         const $target = document.getElementById(target);
 
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         el.classList.toggle('is-active');
         $target.classList.toggle('is-active');
 
@@ -22,4 +24,50 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
+  //*****************
+  //GOOGLE MAPS SETUP
+  //*****************
+
+  function initMap() {
+
+
+    const location = {lat: 51.5002, lng: 0.1332};
+    const map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: location
+    });
+
+    new google.maps.Marker({
+      position: {lat: 51.5002, lng: 0.1332},
+      map: map
+    });
+  }
+
+  if($('#map')[0]) initMap();
+//
+//   function initMap() {
+//
+//     const geocoder = new google.maps.Geocoder();
+//     const address = $('#salonAddress').text();
+//     // gets the latitute and longitude of the restuarant based in its address
+//     geocoder.geocode( { 'address': address}, function(results, status) {
+//       if (status === google.maps.GeocoderStatus.OK) {
+//         const latitude = results[0].geometry.location.lat(); //number
+//         const longitude = results[0].geometry.location.lng(); //number
+//
+//         const location = {lat: latitude, lng: longitude};
+//         const map = new google.maps.Map($('#map')[0], {
+//           zoom: 15,
+//           center: location
+//         });
+//         new google.maps.Marker({
+//           position: location,
+//           map: map
+//         });
+//       }
+//     });
+//   }
+//
+//   if($('#map')[0]) initMap();
+//
 });
