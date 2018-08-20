33a76c8b95716b3f5b10b51e31c89dc7e917d843
Natasha Ramburrun <r_natasha100@hotmail.com>
2018-08-20 10:33:02 +0100

    fixed a bug in the new profile for maps

--- db/seeds.js
+++ db/seeds.js
@@ -1,15 +1,21 @@
 // const mongoose = require('mongoose');
 // mongoose.Promise = require('bluebird');
-// const Portfolio = require('../models/portfolio');
-// const portfolioData = require('./data/portfolio');
-//
+// // const Portfolio = require('../models/portfolio');
+// // const portfolioData = require('./data/portfolio');
+// const User = require('../models/user');
+// const userData = require('./data/user');
 //
 //
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/hair', (err, db) => {
 //   db.dropDatabase();
 //
-//   Portfolio.create(portfolioData)
-//     .then(portfolio => console.log(`${portfolio.length} portfolio created`))
+//   // Portfolio.create(portfolioData)
+//   //   .then(portfolio => console.log(`${portfolio.length} portfolio created`))
+//   //   .catch(err => console.log(err))
+//   //   .finally(() => mongoose.connection.close());
+//
+//   User.create(userData)
+//     .then(user => console.log(`${user.length} user created`))
 //     .catch(err => console.log(err))
 //     .finally(() => mongoose.connection.close());
 //
